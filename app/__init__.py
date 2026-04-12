94b3acd1b3ce844fa0bacbaf5d37a7262a85cb88
alan22222 <alanchacko42@gmail.com>
2026-04-12 19:56:39 +0800

    use environement variable

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,21 @@
+import os
 from flask import Flask
 from flask_mail import Mail
+from dotenv import load_dotenv
 
 mail = Mail()
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'supersecret123'
-
-    app.config["MAIL_SERVER"] = "smtp.gmail.com"
-    app.config["MAIL_PORT"] = 465
-    app.config["MAIL_USE_SSL"] = True
-    app.config["MAIL_USERNAME"] = "alanchacko42@gmail.com"
-    app.config["MAIL_PASSWORD"] = "sjvoychbglsuxpxx"
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT", 465))
+    app.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL", "True") == "True"
+    app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+    app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
     mail.init_app(app)
 
