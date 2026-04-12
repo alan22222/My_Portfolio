9cd5650639f36ee7b4839e271aeb65ec88d940e0
alan22222 <alanchacko42@gmail.com>
2026-04-12 19:41:09 +0800

    added the main pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_mail import Mail
+
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'supersecret123'
+
+    app.config["MAIL_SERVER"] = "smtp.gmail.com"
+    app.config["MAIL_PORT"] = 465
+    app.config["MAIL_USE_SSL"] = True
+    app.config["MAIL_USERNAME"] = "alanchacko42@gmail.com"
+    app.config["MAIL_PASSWORD"] = "sjvoychbglsuxpxx"
+
+    mail.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,55 @@
-from flask import (Blueprint,render_template,url_for)
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .forms import ContactForm
+from flask_mail import Message
+from . import mail
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('about.html')
 
+@main.route("/about")
+def about():
+    return render_template('about.html')
 
-# @main.route("/projects")
-# def projects():
-#     return "My projects page"
+@main.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@main.route("/skills")
+def skills():
+    return render_template('skills.html')
+
+@main.route("/experience")
+def experience():
+    return render_template('experience.html')
+
+@main.route("/education")
+def education():
+    return render_template('education.html')
+
+@main.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        msg = Message(
+            subject="Contact Form Message",
+            sender='alanchacko42@gmail.com',
+            recipients=['alanchacko42@gmail.com'],
+            reply_to=form.email.data
+        )
+        msg.body = f"""
+From: {form.name.data} <{form.email.data}>
+
+{form.message.data}
+"""
+        mail.send(msg)
+        flash("Message sent successfully!")
+        return redirect(url_for('main.contact'))    
+
+    if request.method == 'POST':
+        flash(str(form.errors))
+
+    return render_template('contact.html', form=form)
